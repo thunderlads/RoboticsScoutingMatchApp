aff3fbae61b68211f67f0a2f8ff4d980a6a05f06
akash <akashghoshroytl@gmail.com>
2025-02-05 20:29:54 -0500

    Autonomous -Added savestring for going to teleop page -Fixed only picking one position at a time -AG

--- app/src/main/java/com/example/roboticsscoutingmatchapp/activityAutonomous.java
+++ app/src/main/java/com/example/roboticsscoutingmatchapp/activityAutonomous.java
@@ -2,12 +2,14 @@ package com.example.roboticsscoutingmatchapp;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.Button;
 import android.widget.CheckBox;
 import android.widget.EditText;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -16,6 +18,12 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class activityAutonomous extends AppCompatActivity {
+
+    public void clearGroup(RadioGroup field1, RadioGroup field2){
+        field1.setOnCheckedChangeListener(null);
+        field1.check(-1);
+        field1.setOnCheckedChangeListener((l,w)->clearGroup(field2, field1));
+    }
 
 
 
@@ -107,6 +115,68 @@ public class activityAutonomous extends AppCompatActivity {
         Button backButton = findViewById(R.id.back_button);
         Button saveButton = findViewById(R.id.save_button);
 
+        Toast unfilledMessage = new Toast(this);
+        unfilledMessage.setDuration(Toast.LENGTH_SHORT);
+
+
+        if(!autoSaveString.isEmpty()){
+            // Starting Position | Left starting Position | #ACL1 | #ACL2 | #ACL3 | #ACL4 |
+            // #SCL1 | #SCL2 | #SCL3 | #SCL4 | #attempted algae | #scored algae | #algae removed ||
+            String position = u.untilNextComma(autoSaveString);
+            Log.d(position, position);
+            switch (position){
+                case "Position 1":
+                    position1Button.toggle();
+                    break;
+                case "Position 2":
+                    position2Button.toggle();
+                    break;
+                case "Position 3":
+                    position3Button.toggle();
+                    break;
+                case "Position 4":
+                    position4Button.toggle();
+                    break;
+                case "Position 5":
+                    position5Button.toggle();
+                    break;
+                case "Position 6":
+                    position6Button.toggle();
+                    break;
+            }
+            autoSaveString = u.nextCommaOn(autoSaveString); // Removes starting position
+            if(u.untilNextComma(autoSaveString).equals("True"))
+                leftStarting.toggle();
+            autoSaveString = u.nextCommaOn(autoSaveString); // Removes left starting
+            AC1Field.setText(u.untilNextComma(autoSaveString));
+            autoSaveString = u.nextCommaOn(autoSaveString); // Removes #ACL1
+            AC2Field.setText(u.untilNextComma(autoSaveString));
+            autoSaveString = u.nextCommaOn(autoSaveString); // Removes #ACL2
+            AC3Field.setText(u.untilNextComma(autoSaveString));
+            autoSaveString = u.nextCommaOn(autoSaveString); // Remove #ACL3
+            AC4Field.setText(u.untilNextComma(autoSaveString));
+            autoSaveString = u.nextCommaOn(autoSaveString); // Remove #ACL4
+            RC1Field.setText(u.untilNextComma(autoSaveString));
+            autoSaveString = u.nextCommaOn(autoSaveString); // Remove #SCL1
+            RC2Field.setText(u.untilNextComma(autoSaveString));
+            autoSaveString = u.nextCommaOn(autoSaveString); // Remove #SCL2
+            RC3Field.setText(u.untilNextComma(autoSaveString));
+            autoSaveString = u.nextCommaOn(autoSaveString); // Remove #SCL3
+            RC4Field.setText(u.untilNextComma(autoSaveString));
+            autoSaveString = u.nextCommaOn(autoSaveString); // Remove #SCL4
+            AAField.setText(u.untilNextComma(autoSaveString));
+            autoSaveString = u.nextCommaOn(autoSaveString); // Remove Attempted Algae
+            RAField.setText(u.untilNextComma(autoSaveString));
+            autoSaveString = u.nextCommaOn(autoSaveString); // Remove Scored Algae
+            REAField.setText(u.untilNextComma(autoSaveString));
+            autoSaveString = u.nextCommaOn(autoSaveString); // Remove Algae removed
+        }
+
+
+
+        positionGroup1.setOnCheckedChangeListener((l, w)->clearGroup(positionGroup2, positionGroup1));
+        positionGroup2.setOnCheckedChangeListener((l, w)->clearGroup(positionGroup1, positionGroup2));
+
         // Sets all the buttons to either increment or decrement their respective buttons.
         // Can be simplified. Not now.
         incrementAC1.setOnClickListener((l)->u.incrementText(AC1Field));
@@ -159,7 +229,7 @@ public class activityAutonomous extends AppCompatActivity {
 
             autoInfo += u.getData(AAField) + ",";
             autoInfo += u.getData(RAField) + ",";
-            autoInfo += u.getData(REAField) + ",";
+            autoInfo += u.getData(REAField) + ","; // Algae end
 
             Intent i = new Intent(this, activityPreMatch.class);
             i.putExtra("preMatch", preMatchSaveString);
@@ -168,6 +238,74 @@ public class activityAutonomous extends AppCompatActivity {
             i.putExtra("postMatch", postMatchSaveString);
 
             this.startActivity(i);
+        });
+
+        saveButton.setOnClickListener((l)-> {
+            String response = "";
+
+            if((u.getData(positionGroup1).isEmpty()) && (u.getData(positionGroup2).isEmpty()))
+                response = "Please fill position";
+            else if(u.getData(AC1Field).isEmpty())
+                response = "Please fill Attempted Coral Level 1\n(Input 0 if none)";
+            else if(u.getData(RC1Field).isEmpty())
+                response = "Please fill Scored Coral Level 1\n(Input 0 if none)";
+            else if(u.getData(AC2Field).isEmpty())
+                response = "Please fill Attempted Coral Level 2\n(Input 0 if none)";
+            else if(u.getData(RC2Field).isEmpty())
+                response = "Please fill Scored Coral Level 2\n(Input 0 if none)";
+            else if(u.getData(AC3Field).isEmpty())
+                response = "Please fill Attempted Coral Level 3\n(Input 0 if none)";
+            else if(u.getData(RC3Field).isEmpty())
+                response = "Please fill Scored Coral Level 3\n(Input 0 if none)";
+            else if(u.getData(AC4Field).isEmpty())
+                response = "Please fill Attempted Coral Level 4\n(Input 0 if none)";
+            else if(u.getData(RC4Field).isEmpty())
+                response = "Please fill Scored Coral Level 4\n(Input 0 if none)";
+            else if(u.getData(AAField).isEmpty())
+                response = "Please fill Attempted Algae\n(Input 0 if none)";
+            else if(u.getData(RAField).isEmpty())
+                response = "Please fill Scored Algae\n(Input 0 if none)";
+            else if(u.getData(REAField).isEmpty())
+                response = "Please fill Removed Algae\n(Input 0 if none)";
+            else{
+                String autoInfo = "";
+
+
+                if (u.getData(positionGroup1).isEmpty()) {
+                    autoInfo += u.getData(positionGroup2);
+                } else {
+                    autoInfo += u.getData(positionGroup1);
+                }
+                autoInfo += ","; // Starting position # end
+                autoInfo += u.getData(leftStarting) + ","; // Left starting end
+
+                autoInfo += u.getData(AC1Field) + ",";
+                autoInfo += u.getData(AC2Field) + ",";
+                autoInfo += u.getData(AC3Field) + ",";
+                autoInfo += u.getData(AC4Field) + ","; // Attempted Coral end
+
+                autoInfo += u.getData(RC1Field) + ",";
+                autoInfo += u.getData(RC2Field) + ",";
+                autoInfo += u.getData(RC3Field) + ",";
+                autoInfo += u.getData(RC4Field) + ","; // Scored Coral End
+
+                autoInfo += u.getData(AAField) + ",";
+                autoInfo += u.getData(RAField) + ",";
+                autoInfo += u.getData(REAField) + ","; // Algae end
+
+                Intent i = new Intent(this, activityTeleOp.class);
+                i.putExtra("preMatch", preMatchSaveString);
+                i.putExtra("auto", autoInfo);
+                i.putExtra("teleOp", teleOpSaveString);
+                i.putExtra("postMatch", postMatchSaveString);
+
+                this.startActivity(i);
+            }
+
+            if(!response.isEmpty()){
+                unfilledMessage.setText(response);
+                unfilledMessage.show();
+            }
         });
     }
 }
